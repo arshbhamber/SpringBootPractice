128e5110e761aa7587011fa01991c4c03a8b18e4
Arshdeep Singh <arshbhamber@gmail.com>
2017-04-21 20:43:32 +0530

    first commit

---
+++ src/main/java/com/fake/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.fake.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+public class HelloController{
+
+	
+	@RequestMapping("/")
+	public String index(){
+		
+		return "Welcome to Spring-Boot";	
+
+	}
+
+
+}
